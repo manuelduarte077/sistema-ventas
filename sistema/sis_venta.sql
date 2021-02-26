20279886289119fa893223f37433a42ee0637043
Manuel <carlosd99m@gmail.com>
2021-02-26 10:32:10 -0600

    add base de datos

---
+++ sistema/sis_venta.sql
@@ -0,0 +1,343 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 26-02-2021 a las 16:31:26
+-- Versión del servidor: 5.7.31
+-- Versión de PHP: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sis_venta`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+DROP PROCEDURE IF EXISTS `actualizar_precio_producto`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `actualizar_precio_producto` (IN `n_cantidad` INT, IN `n_precio` DECIMAL(10,2), IN `codigo` INT)  BEGIN
+DECLARE nueva_existencia int;
+DECLARE nuevo_total decimal(10,2);
+DECLARE nuevo_precio decimal(10,2);
+
+DECLARE cant_actual int;
+DECLARE pre_actual decimal(10,2);
+
+DECLARE actual_existencia int;
+DECLARE actual_precio decimal(10,2);
+
+SELECT precio, existencia INTO actual_precio, actual_existencia FROM producto WHERE codproducto = codigo;
+
+SET nueva_existencia = actual_existencia + n_cantidad;
+SET nuevo_total = n_precio;
+SET nuevo_precio = nuevo_total;
+
+UPDATE producto SET existencia = nueva_existencia, precio = nuevo_precio WHERE codproducto = codigo;
+
+SELECT nueva_existencia, nuevo_precio;
+END$$
+
+DROP PROCEDURE IF EXISTS `add_detalle_temp`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_detalle_temp` (`codigo` INT, `cantidad` INT, `token_user` VARCHAR(50))  BEGIN
+DECLARE precio_actual decimal(10,2);
+SELECT precio INTO precio_actual FROM producto WHERE codproducto = codigo;
+INSERT INTO detalle_temp(token_user, codproducto, cantidad, precio_venta) VALUES (token_user, codigo, cantidad, precio_actual);
+SELECT tmp.correlativo, tmp.codproducto, p.descripcion, tmp.cantidad, tmp.precio_venta FROM detalle_temp tmp INNER JOIN producto p ON tmp.codproducto = p.codproducto WHERE tmp.token_user = token_user;
+END$$
+
+DROP PROCEDURE IF EXISTS `data`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `data` ()  BEGIN
+DECLARE usuarios int;
+DECLARE clientes int;
+DECLARE proveedores int;
+DECLARE productos int;
+DECLARE ventas int;
+SELECT COUNT(*) INTO usuarios FROM usuario;
+SELECT COUNT(*) INTO clientes FROM cliente;
+SELECT COUNT(*) INTO proveedores FROM proveedor;
+SELECT COUNT(*) INTO productos FROM producto;
+SELECT COUNT(*) INTO ventas FROM factura WHERE fecha > CURDATE();
+
+SELECT usuarios, clientes, proveedores, productos, ventas;
+
+END$$
+
+DROP PROCEDURE IF EXISTS `del_detalle_temp`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `del_detalle_temp` (`id_detalle` INT, `token` VARCHAR(50))  BEGIN
+DELETE FROM detalle_temp WHERE correlativo = id_detalle;
+SELECT tmp.correlativo, tmp.codproducto, p.descripcion, tmp.cantidad, tmp.precio_venta FROM detalle_temp tmp INNER JOIN producto p ON tmp.codproducto = p.codproducto WHERE tmp.token_user = token;
+END$$
+
+DROP PROCEDURE IF EXISTS `procesar_venta`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `procesar_venta` (IN `cod_usuario` INT, IN `cod_cliente` INT, IN `token` VARCHAR(50))  BEGIN
+DECLARE factura INT;
+DECLARE registros INT;
+DECLARE total DECIMAL(10,2);
+DECLARE nueva_existencia int;
+DECLARE existencia_actual int;
+
+DECLARE tmp_cod_producto int;
+DECLARE tmp_cant_producto int;
+DECLARE a int;
+SET a = 1;
+
+CREATE TEMPORARY TABLE tbl_tmp_tokenuser(
+	id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cod_prod BIGINT,
+    cant_prod int);
+SET registros = (SELECT COUNT(*) FROM detalle_temp WHERE token_user = token);
+IF registros > 0 THEN
+INSERT INTO tbl_tmp_tokenuser(cod_prod, cant_prod) SELECT codproducto, cantidad FROM detalle_temp WHERE token_user = token;
+INSERT INTO factura (usuario,codcliente) VALUES (cod_usuario, cod_cliente);
+SET factura = LAST_INSERT_ID();
+
+INSERT INTO detallefactura(nofactura,codproducto,cantidad,precio_venta) SELECT (factura) AS nofactura, codproducto, cantidad,precio_venta FROM detalle_temp WHERE token_user = token;
+WHILE a <= registros DO
+	SELECT cod_prod, cant_prod INTO tmp_cod_producto,tmp_cant_producto FROM tbl_tmp_tokenuser WHERE id = a;
+    SELECT existencia INTO existencia_actual FROM producto WHERE codproducto = tmp_cod_producto;
+    SET nueva_existencia = existencia_actual - tmp_cant_producto;
+    UPDATE producto SET existencia = nueva_existencia WHERE codproducto = tmp_cod_producto;
+    SET a=a+1;
+END WHILE;
+SET total = (SELECT SUM(cantidad * precio_venta) FROM detalle_temp WHERE token_user = token);
+UPDATE factura SET totalfactura = total WHERE nofactura = factura;
+DELETE FROM detalle_temp WHERE token_user = token;
+TRUNCATE TABLE tbl_tmp_tokenuser;
+SELECT * FROM factura WHERE nofactura = factura;
+ELSE
+SELECT 0;
+END IF;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+CREATE TABLE IF NOT EXISTS `cliente` (
+  `idcliente` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` int(8) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` int(15) NOT NULL,
+  `direccion` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`idcliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `cliente`
+--
+
+INSERT INTO `cliente` (`idcliente`, `dni`, `nombre`, `telefono`, `direccion`, `usuario_id`) VALUES
+(1, 123545, 'Pubico en general', 925491523, 'Lima', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `configuracion`
+--
+
+DROP TABLE IF EXISTS `configuracion`;
+CREATE TABLE IF NOT EXISTS `configuracion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dni` int(11) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `razon_social` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `email` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `direccion` text COLLATE utf8_spanish_ci NOT NULL,
+  `igv` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `configuracion`
+--
+
+INSERT INTO `configuracion` (`id`, `dni`, `nombre`, `razon_social`, `telefono`, `email`, `direccion`, `igv`) VALUES
+(1, 2580, 'Vida Informático', 'Vida Informático', 925491523, 'naju@vidainformatico.com', 'Lima - Perú', '1.18');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallefactura`
+--
+
+DROP TABLE IF EXISTS `detallefactura`;
+CREATE TABLE IF NOT EXISTS `detallefactura` (
+  `correlativo` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nofactura` bigint(20) NOT NULL,
+  `codproducto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_venta` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`correlativo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_temp`
+--
+
+DROP TABLE IF EXISTS `detalle_temp`;
+CREATE TABLE IF NOT EXISTS `detalle_temp` (
+  `correlativo` int(11) NOT NULL AUTO_INCREMENT,
+  `token_user` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `codproducto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio_venta` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`correlativo`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `entradas`
+--
+
+DROP TABLE IF EXISTS `entradas`;
+CREATE TABLE IF NOT EXISTS `entradas` (
+  `correlativo` int(11) NOT NULL AUTO_INCREMENT,
+  `codproducto` int(11) NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `cantidad` int(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`correlativo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+DROP TABLE IF EXISTS `factura`;
+CREATE TABLE IF NOT EXISTS `factura` (
+  `nofactura` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `usuario` int(11) NOT NULL,
+  `codcliente` int(11) NOT NULL,
+  `totalfactura` decimal(10,2) NOT NULL,
+  `estado` int(11) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`nofactura`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+CREATE TABLE IF NOT EXISTS `producto` (
+  `codproducto` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `proveedor` int(11) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `existencia` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`codproducto`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`codproducto`, `descripcion`, `proveedor`, `precio`, `existencia`, `usuario_id`) VALUES
+(1, 'Laptop lenovo', 1, '1560.00', 49, 2),
+(2, 'Televisor', 1, '2500.00', 79, 1),
+(6, 'Impresora', 1, '800.00', 0, 1),
+(7, 'Gaseosa', 3, '1500.00', 5, 1),
+(8, 'ddd', 3, '25.00', 23, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+DROP TABLE IF EXISTS `proveedor`;
+CREATE TABLE IF NOT EXISTS `proveedor` (
+  `codproveedor` int(11) NOT NULL AUTO_INCREMENT,
+  `proveedor` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `contacto` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `direccion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`codproveedor`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `proveedor`
+--
+
+INSERT INTO `proveedor` (`codproveedor`, `proveedor`, `contacto`, `telefono`, `direccion`, `usuario_id`) VALUES
+(1, 'Open Services', '965432143', 9645132, 'Lima', 2),
+(3, 'Lineo', '25804', 9865412, 'Lima', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+DROP TABLE IF EXISTS `rol`;
+CREATE TABLE IF NOT EXISTS `rol` (
+  `idrol` int(11) NOT NULL AUTO_INCREMENT,
+  `rol` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  PRIMARY KEY (`idrol`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `rol`
+--
+
+INSERT INTO `rol` (`idrol`, `rol`) VALUES
+(1, 'Administrador'),
+(2, 'Vendedor');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `idusuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `correo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `usuario` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `clave` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `rol` int(11) NOT NULL,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `nombre`, `correo`, `usuario`, `clave`, `rol`) VALUES
+(1, 'Thanos', 'thanos@gmail.com', 'thanos', '21232f297a57a5a743894a0e4a801fc3', 1),
+(6, 'Maria Perez Miranda', 'maria@gmail.com', 'maria', '263bce650e68ab4e23f28263760b9fa5', 3);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
